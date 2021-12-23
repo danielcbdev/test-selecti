e67727993d4d933ae070529829d93c4682a5f34c
Daniel Carvalho <danielcbdev@gmail.com>
2021-12-23 14:03:56 -0300

    corrections

--- lib/repository/login_repository.dart
+++ lib/repository/login_repository.dart
@@ -4,8 +4,15 @@ import 'package:http/http.dart' as http;
 
 class LoginRepository {
   Map<String, dynamic>? getResponse(){
+    Map<String, dynamic> map = {
+      "email": "email.com",
+      "password": "123456",
+    };
     try{
-      Future<http.Response> response = http.get(Uri.parse("https://jsonplaceholder.typicode.com/users/"));
+      Future<http.Response> response = http.post(
+        Uri.parse("https://jsonplaceholder.typicode.com/users/"),
+        body: map
+      );
       response.then((value) {
         if (value.statusCode == 200) {
           Map<String, dynamic> data = json.decode(value.body);
